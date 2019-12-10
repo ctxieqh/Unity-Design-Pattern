89f17349d2cc560bf9c53e08afe4c5128b22b012
Lijie <2068950013@qq.com>
2019-12-10 14:33:03 +0800

    添加反射实例

--- Assets/Creational Patterns/Prototype Pattern/Example2/DynamicProxy.cs
+++ Assets/Creational Patterns/Prototype Pattern/Example2/DynamicProxy.cs
@@ -4,15 +4,26 @@ using System;
 using System.Runtime.Remoting.Proxies;
 using System.Runtime.Remoting.Messaging;
 using System.Reflection;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.InteropServices;
 
 public class DynamicProxy : MonoBehaviour
 {
     // Use this for initialization
     void Start()
     {
-        IMyInterface intf = LoggingProxy<IMyInterface>.Create(new MyClass());
-        intf.MyProcedure();
-        intf.Function();
+        //反射实例
+        MyClass myClass =(MyClass)Activator.CreateInstance(typeof(MyClass));  //创建对象实例 object obj=Activator.CreateInstance(type);  //创建对象实例
+        ///获取字段必须指定 BindingFlags.Instance 或 BindingFlags.Static，类为static则指定BindingFlags.Instance
+        FieldInfo fileInfo = myClass.GetType().GetField("s1",BindingFlags.NonPublic|BindingFlags.Instance);//获取私有字段
+        PropertyInfo[] propertyInfos=  myClass.GetType().GetProperties();//获取属性
+        MemberInfo[] memberInfos =myClass.GetType().GetMembers();//获取成员变量(字段+属性)
+        fileInfo.SetValue(myClass, "s2");//修改字段值
+        MethodInfo methodInfo = myClass.GetType().GetMethod("MyProcedure");
+        methodInfo.Invoke(methodInfo,null);//实例方法必须依附于对象实例才能执行
+
+        IMyInterface intf = LoggingProxy<IMyInterface>.Create(myClass);
+        intf.MyProcedure();//
     }
 
 }
@@ -24,6 +35,14 @@ public class DynamicProxy : MonoBehaviour
 
 class MyClass : IMyInterface
 {
+    private string s1;
+    public string S1 { get { return s1; } }
+    public string ps1;
+    public MyClass()
+    {
+        s1 = "s1";
+        ps1 = "ps1";
+    }
     public void Function()
     {
         throw new NotImplementedException();
@@ -34,7 +53,10 @@ class MyClass : IMyInterface
         Console.WriteLine("Hello World");
     }
 }
-
+/// <summary>
+///泛型类加载代理
+/// </summary>
+/// <typeparam name="T">必须为抽象接口</typeparam>
 public class LoggingProxy<T> : RealProxy
 {
     private readonly T _instance;
@@ -43,7 +65,12 @@ public class LoggingProxy<T> : RealProxy
     {
         _instance = instance;
     }
-
+    /// <summary>
+    /// //返回 RealProxy 的当前实例的透明代理。
+    /// </summary>
+    /// <param name="instance">要代理的实例1
+    /// </param>
+    /// <returns></returns>
     public static T Create(T instance)
     {
         return (T)new LoggingProxy<T>(instance).GetTransparentProxy();
@@ -53,18 +80,17 @@ public class LoggingProxy<T> : RealProxy
     {
         var methodCall = (IMethodCallMessage)msg;
         var method = (MethodInfo)methodCall.MethodBase;
-
+       
         try {
-            Console.WriteLine("Before invoke: " + method.Name);
+            Console.WriteLine("Before invoke: " + method.Name);//在调用方法前做记录
             var result = method.Invoke(_instance, methodCall.InArgs);
-            Console.WriteLine("After invoke: " + method.Name);
+            Console.WriteLine("After invoke: " + method.Name);//在调用方法后做记录
             return new ReturnMessage(result, null, 0, methodCall.LogicalCallContext, methodCall);
         } catch (Exception e) {
             Console.WriteLine("Exception: " + e);
             if (e is TargetInvocationException && e.InnerException != null) {
                 return new ReturnMessage(e.InnerException, msg as IMethodCallMessage);
             }
-
             return new ReturnMessage(e, msg as IMethodCallMessage);
         }
     }
